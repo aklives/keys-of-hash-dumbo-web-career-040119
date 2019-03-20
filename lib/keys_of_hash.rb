9070f50de5a08d1a6ad6a865efc1cc84074e211a
Adam Kearney <aklives@hotmail.com>
2019-03-20 03:50:04 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,7 @@
 class Hash
-  def keys_of(arguments*)
+  def keys_of(*arguments)
     arr = []
-    map {|k, v| if arguments.include? v
-            arr << k}
+    map {|k, v| if arguments.include?(v) arr << k}
 
   return arr
   end
