698278bd567edd91257e17908266a106a8a57282
Adam Kearney <aklives@hotmail.com>
2019-03-20 04:25:57 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(*args)
 
-      map {|key, value| arg.include?(value) ? (key << arr) : nil }
+      map {|key, value| arg.include?(value) ? (key << arr) : nil }.compact
 
 
 
